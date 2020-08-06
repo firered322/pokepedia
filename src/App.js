7a71c96ed88ae74933e51e2ed94050b4106fbb67
Atul <atulsingh1396@gmail.com>
2020-08-06 07:35:15 +0530

    arrangind child components inside the router parent

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
-import Landing from './components/landing/Landing'
-import Pokedex from './components/pokedex/Pokedex'
-import Battler from './components/battler/Battler'
+import Landing from "./components/landing/Landing";
+import Pokedex from "./components/pokedex/Pokedex";
+import Battler from "./components/battler/Battler";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <div className="container">
-        <Navbar />
         <Route exact path="/" component={Landing} />
         <Route exact path="/pokedex" component={Pokedex} />
         <Route exact path="/battler" component={Battler} />
--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -4,25 +4,23 @@ import logo from "../../assets/pokemon.svg";
 
 const Landing = () => {
   return (
-    <div className="landing-container">
-      <div className="landing-parent">
-        <div className="landing-child">
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi natus
-            consequatur fuga reprehenderit. Atque cupiditate quisquam ut amet
-            aspernatur! Non eveniet animi similique officia rerum aspernatur sed
-            perferendis a iure?
-          </p>
-          <div className="cta-btn-holder">
-            <button>Explore Pokédex</button>
-            <button>Explore Rankings</button>
-          </div>
+    <div className="landing-parent">
+      <div className="landing-child">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi natus
+          consequatur fuga reprehenderit. Atque cupiditate quisquam ut amet
+          aspernatur! Non eveniet animi similique officia rerum aspernatur sed
+          perferendis a iure?
+        </p>
+        <div className="cta-btn-holder">
+          <button>Explore Pokédex</button>
+          <button>Explore Rankings</button>
         </div>
+      </div>
 
-        <div className="landing-child">
-          <div>
-            <img src={logo} className="spinner-logo" alt="logo" />
-          </div>
+      <div className="landing-child">
+        <div>
+          <img src={logo} className="spinner-logo" alt="logo" />
         </div>
       </div>
     </div>
