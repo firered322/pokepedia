c55ea774a233443ad39d6f7197e42c4304abf9b0
Atul <atulsingh1396@gmail.com>
2020-08-06 06:56:09 +0530

    added landing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import Landing from './components/landing/Landing'
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Landing />
     </div>
   );
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,8 +4,8 @@ import './Navbar.css'
 const Navabar = () => {
   return (
     <nav>
-      <div className="nav-logo">PogoPedia</div>
-      <div>
+      <div className="nav-logo">Header</div>
+      <div className="nav-links-holder">
         <ul className="nav-links">
           <li className="nav-link">Home</li>
           <li className="nav-link">PokeDex</li>
